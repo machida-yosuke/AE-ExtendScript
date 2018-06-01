f9c71f74837062761609b884c80ddf734e93cf56
machida-yosuke <machida-yosuke@kayac.com>
2018-06-01 20:07:46 +0900

    キューに突っ込むまで

--- src/main.js
+++ src/main.js
@@ -1,19 +1,65 @@
-import * as json from "./1000000.json"
+import * as json from "./10000.json"
 
-const count = json["name_all"].length
-let name_array = []
+const COMP_PROP = {
+  'compWidth': 1920,
+  'compHeight': 1080,
+  'pixelAspect': 1.0,
+  'compFps': 60,
+  'compTime': 10 //秒
+};
 
-const compName = "staffroll"
-const creComp = app.project.items.addComp(compName,1920,1080,1.0,600,60)
+const TEXT_PROP = {
+  'font': "Osaka",
+  'size': 20,
+  'lineHeight': 80,
+  'color': [1, 1, 1]
+};
 
+// ここらへんはよくわからん
+const offset = 17;
+
+const count = 100;
+let name_array = [];
+let createCount = 0;
 
 json["name_all"].forEach((item, index) =>{
-  if (index % 10 == 0) {
+  if (index % 10 == 0 && index != 0) {
     name_array.push(json["name_all"][index]["name"] + "\n")
+  } else {
+    name_array.push(json["name_all"][index]["name"])
   }
-  name_array.push(json["name_all"][index]["name"])
+
+  if (index % count == 0 && index != 0) {
+    createCount += 1;
+    const compName = "staffroll" + createCount;
+    const comp = app.project.items.addComp(
+      compName,
+      COMP_PROP.compWidth,
+      COMP_PROP.compHeight,
+      COMP_PROP.pixelAspect,
+      COMP_PROP.compTime,
+      COMP_PROP.compFps
+    );
+
+    const textLayer = comp.layers.addText(name_array.join("  "));
+    var textLayer_TextProp = textLayer.property("Source Text");
+    var textLayer_TextDocument = textLayer_TextProp.value;
+    textLayer_TextDocument.resetCharStyle();
+    textLayer_TextDocument.fillColor = TEXT_PROP.color;
+    textLayer_TextDocument.font = TEXT_PROP.font;
+    textLayer_TextDocument.leading = TEXT_PROP.lineHeight;
+    textLayer_TextDocument.fontSize = TEXT_PROP.size;
+    textLayer_TextProp.setValue(textLayer_TextDocument);
+
+    const y =textLayer.sourceRectAtTime(0, false).height;
+    textLayer('position').setValue([COMP_PROP.compWidth / 2, COMP_PROP.compHeight / 2]);
+    textLayer('anchorPoint').setValue([0, y / 2]);
+
+    textLayer('position').setValueAtTime(0, [COMP_PROP.compWidth / 2, COMP_PROP.compHeight + y / 2 + offset ])
+    textLayer('position').setValueAtTime(COMP_PROP.compTime, [COMP_PROP.compWidth / 2, -y / 2 + offset])
+    app.project.renderQueue.items.add(comp)
+    name_array = []
+  }
 })
 
-creComp.layers.addText(name_array.join(""));
-
-alert(name_array.join(""))
+alert('owari');
